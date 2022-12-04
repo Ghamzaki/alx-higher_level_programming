15f963928cf2689bff343dc547405cd74b7ab68e
Ghamzaki <uthmanghamzaki@gmail.com>
2022-12-04 12:43:31 -0800

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    
+    num = len(sys.argv) - 1
+    total = 0
+    for i in range(num):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
