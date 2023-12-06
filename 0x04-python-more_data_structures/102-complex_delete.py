99d6f4262f2d11785ce231415458da67d079b889
Ghamzaki <uthmanghamzaki@gmail.com>
2023-12-06 20:21:15 +0000

    lambda

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    def complex_delete(a_dictionary, value):
     list_keys = list(a_dictionary.keys())
 
     for value_dic in list_keys:
@@ -8,3 +7,4 @@ def complex_delete(a_dictionary, value):
             del a_dictionary[value_dic]
 
     return (a_dictionary)
+    
